9d9061eacfb8a86b2c3ee9366dd07b3c5252424d
Albert Attard <albert.attard@oracle.com>
2023-07-29 00:37:11 +0200

    Use proper annotation

--- src/main/java/gourami/register/RegisterController.java
+++ src/main/java/gourami/register/RegisterController.java
@@ -1,12 +1,12 @@
 package gourami.register;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Qualifier("/register")
+@RequestMapping("/register")
 public class RegisterController {
 
     @PostMapping("/")
